4ef49618a822d53beb58dd5d9f173ef9daa21faf
Xi Wu <wuxi9007@gmail.com>
2020-04-26 23:19:01 -0400

    fix js css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,11 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_controller.perform_caching = true
-  config.serve_static_assets = true
-  
+
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Apache or NGINX already handles this
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -26,6 +25,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
